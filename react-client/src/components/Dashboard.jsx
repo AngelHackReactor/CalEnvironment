1646564222712a8e762400af046ead43fdfcee34
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-07-29 16:24:08 -0700

    (feat) remove dashboard bg image

--- react-client/src/components/Dashboard.jsx
+++ react-client/src/components/Dashboard.jsx
@@ -18,35 +18,38 @@ class Dashboard extends React.Component {
   }
   render() {
     return (
-      <div className="container">
-
-        <div className="card-deck">
-          <div className="card" style={style.cards}>
-            <img className="card-img-top" src="//placehold.it/800x400" alt="Card image cap"/>
-            <div className="card-block">
-              <h4 className="card-title">Air</h4>
-              <p className="card-text"></p>
+      <div className="dashboard-bg">
+        <div className="container">
+          {/* Air Card */}
+          <div className="card-deck">
+            <div className="card" style={style.cards}>
+              <img className="card-img-top mx-auto d-block" src="https://res.cloudinary.com/jescobedo/image/upload/v1501369711/air_duuqdf.svg" width="200px" alt="Card image cap"/>
+              <div className="card-block">
+                <h4 className="card-title">Air</h4>
+                <p className="card-text"></p>
+              </div>
             </div>
-          </div>
 
-          <div className="card" style={style.cards}>
-            <img className="card-img-top" src="//placehold.it/800x400" alt="Card image cap"/>
-            <div className="card-block">
-              <h4 className="card-title">Water</h4>
-              <p className="card-text"></p>
+            {/* Water Card */}
+            <div className="card" style={style.cards}>
+              <img className="card-img-top mx-auto d-block" src="https://res.cloudinary.com/jescobedo/image/upload/v1501369805/water_ypu3ev.svg" width="200px" alt="Card image cap"/>
+              <div className="card-block">
+                <h4 className="card-title">Water</h4>
+                <p className="card-text"></p>
+              </div>
             </div>
-          </div>
 
-          <div className="card" style={style.cards}>
-            <img className="card-img-top" src="//placehold.it/800x400" alt="Card image cap"/>
-            <div className="card-block">
-              <h4 className="card-title">Waste</h4>
-              <p className="card-text"></p>
+            {/* Waste Card */}
+            <div className="card" style={style.cards}>
+              <img className="card-img-top mx-auto d-block" src="https://res.cloudinary.com/jescobedo/image/upload/v1501369896/waste_tjfsv3.svg" width="200px"alt="Card image cap"/>
+              <div className="card-block">
+                <h4 className="card-title">Waste</h4>
+                <p className="card-text"></p>
+              </div>
             </div>
-          </div>
 
+          </div>
         </div>
-
       </div>
     )
   }

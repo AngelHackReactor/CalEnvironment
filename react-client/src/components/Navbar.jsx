d015192de70edf069ffe12bc52467eeaa8928fff
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-07-29 23:39:09 -0700

    (rebase) rebase last PR

--- react-client/src/components/Navbar.jsx
+++ react-client/src/components/Navbar.jsx
@@ -27,12 +27,12 @@ class Navbar extends React.Component {
   render() {
     return (
        <div className="Navigation">
-        <nav className="navbar navbar-toggleable-md navbar-dark fixed-top transition">
+        <nav className="navbar navbar-toggleable-md navbar-light fixed-top transition">
             <div className="container">
                 <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav1" aria-controls="navbarNav1" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <a className="navbar-brand navbar-hero" style={{color: 'grey'}} href="/">SAFE CALIFORNIA</a>
+                <a className="navbar-brand navbar-hero" href="/">SAFE CALIFORNIA</a>
                 <div className="collapse navbar-collapse" id="navbarNav1">
                     <ul className="navbar-nav ml-auto">
                       <li className="nav-item">

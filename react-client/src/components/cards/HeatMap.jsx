ba45aed65a7e15f9c370fbb080b1210894802166
Jeffrey Chen <jeffreychen41@gmail.com>
2017-07-30 10:37:25 -0700

    Added Map

--- react-client/src/components/cards/HeatMap.jsx
+++ react-client/src/components/cards/HeatMap.jsx
@@ -8,7 +8,7 @@ import { getData } from '../../actions';
 // Set your mapbox access token here
 // Viewport settings that is shared between mapbox and deck.gl
 const viewport = {
-   zoom: 5,
+   zoom: 10,
    pitch: 60,
    bearing: 20
 }
@@ -31,7 +31,8 @@ class HeatMap extends React.Component {
       <MapGL {...viewport}
         longitude= {this.props.lon}
         latitude= {this.props.lat}
-        minWidth={300}
+        Width={500}
+        height={400}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxApiAccessToken={"pk.eyJ1IjoiamVmZmMxMiIsImEiOiJjajVxc2Q5bzcwbjRnMzNzZDZ5dTVwemh5In0.VLNgP3mIY1ufVRmjPz7-eA"}
         perspectiveEnabled={true}>
@@ -45,8 +46,8 @@ class HeatMap extends React.Component {
 const mapStateToProps = ({ location }) => {
   return {
     location: location.location,
-    lat: location.location.lat,
-    lon: location.location.lon
+    lat: location.lat,
+    lon: location.lon
   }
 }
 

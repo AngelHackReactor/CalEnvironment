4b421a62f8e0059a0e19573d8296b7322b64ae04
Vincent Van Buskirk <vvanbuskirk805@gmail.com>
2017-07-29 21:57:04 -0700

    Percentiles added to database

--- db/migrations/initial.js
+++ db/migrations/initial.js
@@ -5,11 +5,17 @@ exports.up = function(knex, promise) {
 			table.bigInteger('census_tract_id').nullable();
 			table.string('county').nullable();
 			table.decimal('drinking_water').nullable();
+			table.decimal('drinking_water_percentile').nullable();
 			table.decimal('pm_2point5').nullable();
+			table.decimal('pm_2point5_percentile').nullable();
 			table.decimal('diesel_pm').nullable();
+			table.decimal('diesel_pm_percentile').nullable();
 			table.decimal('groundwater_threats').nullable();
+			table.decimal('groundwater_threat_percentile').nullable();
 			table.decimal('solid_waste').nullable();
+			table.decimal('solid_waste_percentile').nullable();
 			table.decimal('hazardous_waste').nullable();
+			table.decimal('hazardous_waste_percentile').nullable();
 			table.decimal('zip_code').nullable();
 			table.string('city').nullable();
 		})

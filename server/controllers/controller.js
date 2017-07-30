3835f080d42c5da45c92e1c23dd6f0fd4d7306c6
Alan Zheng <aszheng12@gmail.com>
2017-07-29 21:00:58 -0700

    completed getCensusTract endpoint

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -12,9 +12,9 @@ module.exports.landing = (req, res) => {
 }
 
 module.exports.getCensusTract = (req, res) => {
-  let addressStr = req.body;
+  // let addressStr = req.body;
 
-  // let addressStr = '12 Amberwood Circle, San Francisco'
+  let addressStr = '12 Amberwood Circle, San Francisco'
   request(`https://geocoding.geo.census.gov/geocoder/geographies/onelineaddress?address=${addressStr}&benchmark=Public_AR_Census2010&vintage=Census2010_Census2010&layer s=14&format=json`, 
     (err, resp, body) => {
       let censusTract = JSON.parse(body).result.addressMatches[0].geographies["Census Blocks"][0].GEOID

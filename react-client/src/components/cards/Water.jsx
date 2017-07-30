3c2e34debd186a4212551b109be098b037d8ae7d
Jeffrey Chen <jeffreychen41@gmail.com>
2017-07-29 23:56:57 -0700

    new graphs

--- react-client/src/components/cards/Water.jsx
+++ react-client/src/components/cards/Water.jsx
@@ -9,46 +9,44 @@ class Water extends React.Component {
   componentDidMount() {
     let chart = c3.generate({
       bindto: '#water',
-      type:'line',
       data: {
         columns: [
-            ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 50, 20, 10, 40, 15, 25]
-        ]
-      },
-      size: {
-        height: 200
-      },
-      axis: {
-        y: {
+            ['days', 0]
+        ],
+        type: 'bar',
+        labels: true
+        },
+        size: {
+          height: 100
+        },
+        axis: {
+          rotated: true,
+          x: {
+              show: false
+          },
+          y: {
+              show: false,
+              max: 100
+          }
+        },
+        legend: {
+          hide: true
+        },
+        tooltip: {
           show: false
+        },
+        bar: {
+          width: 50
         }
-      }
     });
 
     setTimeout(function () {
-      chart.load({
-        columns: [
-          ['data1', 230, 190, 300, 500, 300, 400]
-        ]
-      });
+    chart.load({
+       columns: [
+           ['days', 100-30]
+       ]
+    });
     }, 1000);
-
-    setTimeout(function () {
-      chart.load({
-        columns: [
-          ['data3', 130, 150, 200, 300, 200, 100]
-        ]
-      });
-    }, 1500);
-
-    setTimeout(function () {
-      chart.unload({
-        ids: 'data1'
-      });
-    }, 2000);
-
-    
   }
   render() {
     return (

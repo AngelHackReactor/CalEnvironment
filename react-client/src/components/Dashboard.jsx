9081ef1833379a0cac7bef2b3da6da5cae8494e6
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-07-30 00:24:49 -0700

    (styling) update dashboard styling

--- react-client/src/components/Dashboard.jsx
+++ react-client/src/components/Dashboard.jsx
@@ -38,13 +38,13 @@ class Dashboard extends React.Component {
             </div>
           </div>*/}
             <div className="jumbotron city-name">
-              <h1>San Francisco, CA</h1>
+              <h1>{this.props.location}</h1>
             </div>
 
             <div className="cards">
                   {/* Air Card */}
                   <div className="card-deck">
-                    <div className="card" style={style.cards}>
+                    <div className="card card-air" style={style.cards}>
                       <img className="card-img-top mx-auto d-block" src="https://res.cloudinary.com/jescobedo/image/upload/v1501393341/air_jcqxns.svg" width="200px" alt="Card image cap"/>
                       <div className="card-block">
                         <h4 className="card-title text-center">Air</h4>
@@ -54,7 +54,7 @@ class Dashboard extends React.Component {
                     </div>
 
                     {/* Water Card */}
-                    <div className="card" style={style.cards}>
+                    <div className="card card-water" style={style.cards}>
                       <img className="card-img-top mx-auto d-block" src="https://res.cloudinary.com/jescobedo/image/upload/v1501393344/water_tnf1h1.svg" width="200px" alt="Card image cap"/>
                       <div className="card-block">
                         <h4 className="card-title text-center">Water</h4>
@@ -64,7 +64,7 @@ class Dashboard extends React.Component {
                     </div>
 
                     {/* Waste Card */}
-                    <div className="card" style={style.cards}>
+                    <div className="card card-waste" style={style.cards}>
                       <img className="card-img-top mx-auto d-block" src="https://res.cloudinary.com/jescobedo/image/upload/v1501393348/waste_evxmx6.svg" width="200px"alt="Card image cap"/>
                       <div className="card-block">
                         <h4 className="card-title text-center">Waste</h4>
